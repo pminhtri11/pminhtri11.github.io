b1c0197540353507484749658d323e6d0905b7f6
Tri Phan <minhtri11tk@gmail.com>
2019-10-08 23:00:14 -0500

    Update Portfolio

---
+++ assets/javascript/logic.js
@@ -0,0 +1,13 @@
+$(".soloProject").on("click", function()
+{
+    $(".solo-project").show();
+    $(".group-project").hide();
+})
+$(".groupProject").on("click", function()
+{
+    $(".solo-project").hide();
+    $(".group-project").show();
+})
+
+
+   
